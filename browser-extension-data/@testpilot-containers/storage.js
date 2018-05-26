390e52932400a58af72af522876c3b4032972ee8
Will JS <will.js@protonmail.com>
2018-05-26 18:14:58 +0000

    Initial commit

---
+++ browser-extension-data/@testpilot-containers/storage.js
@@ -0,0 +1,684 @@
+{
+  "browserActionBadgesClicked": [
+    "6.0.0"
+  ],
+  "onboarding-stage": 5,
+  "containerTabsOpened": 996,
+  "siteContainerMap@@_www.reddit.com": {
+    "userContextId": "7",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_github.com": {
+    "userContextId": "6",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.amazon.com": {
+    "userContextId": "8",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.ebay.com": {
+    "userContextId": "11",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.davannis.com": {
+    "userContextId": "12",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_davannisgoldenvalley.foodtecsolutions.com": {
+    "userContextId": "12",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_adfs.dunwoody.edu": {
+    "userContextId": "9",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_dunwoody.instructure.com": {
+    "userContextId": "9",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.netflix.com": {
+    "userContextId": "13",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_wiki.archlinux.org": {
+    "userContextId": "14",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.archlinux.org": {
+    "userContextId": "14",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_bugs.archlinux.org": {
+    "userContextId": "14",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_aur.archlinux.org": {
+    "userContextId": "14",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_bbs.archlinux.org": {
+    "userContextId": "14",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_gist.github.com": {
+    "userContextId": "6",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_pinboard.in": {
+    "userContextId": "15",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_getpocket.com": {
+    "userContextId": "16",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.codewars.com": {
+    "userContextId": "17",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_mail.protonmail.com": {
+    "userContextId": "18",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_protonmail.com": {
+    "userContextId": "18",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.datacamp.com": {
+    "userContextId": "19",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.nexusmods.com": {
+    "userContextId": "20",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_ifttt.com": {
+    "userContextId": "21",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_feedly.com": {
+    "userContextId": "22",
+    "neverAsk": true
+  },
+  "achievements": [
+    {
+      "name": "manyContainersOpened",
+      "done": true
+    }
+  ],
+  "siteContainerMap@@_www.cargurus.com": {
+    "userContextId": "23",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.udemy.com": {
+    "userContextId": "24",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.massdrop.com": {
+    "userContextId": "10",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_signin.ebay.com": {
+    "userContextId": "11",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.papajohns.com": {
+    "userContextId": "28",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_my.dunwoody.edu": {
+    "userContextId": "9",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_minneapolis.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_accounts.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_stcloud.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_marshall.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_rmn.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_mankato.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_duluth.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_brainerd.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_bemidji.craigslist.org": {
+    "userContextId": "29",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_connect.ebay.com": {
+    "userContextId": "11",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_my.ebay.com": {
+    "userContextId": "11",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_twitter.com": {
+    "userContextId": "30",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.capitalone.com": {
+    "userContextId": "25",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_verified.capitalone.com": {
+    "userContextId": "25",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_myaccounts.capitalone.com": {
+    "userContextId": "25",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_controlpanel.newshosting.com": {
+    "userContextId": "32",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.newshosting.com": {
+    "userContextId": "32",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_support.newshosting.com": {
+    "userContextId": "32",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.pizzahut.com": {
+    "userContextId": "33",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.fidelity.com": {
+    "userContextId": "34",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_oltx.fidelity.com": {
+    "userContextId": "34",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_scs.fidelity.com": {
+    "userContextId": "34",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_statements.fidelity.com": {
+    "userContextId": "34",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_servicemessages.fidelity.com": {
+    "userContextId": "34",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.hiway.org": {
+    "userContextId": "35",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_onlinebanking.hiway.org": {
+    "userContextId": "35",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_wiki.step-project.com": {
+    "userContextId": "27",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_forum.step-project.com": {
+    "userContextId": "27",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_accounts.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_mail.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_myaccount.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_drive.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_photos.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_developers.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_bitwarden.com": {
+    "userContextId": "36",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_vault.bitwarden.com": {
+    "userContextId": "36",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_help.bitwarden.com": {
+    "userContextId": "36",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_firebase.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_console.firebase.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_cloud.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_console.cloud.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_inbox.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_calendar.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_play.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_keep.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_contacts.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_docs.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_hangouts.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_domains.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_takeout.google.com": {
+    "userContextId": "31",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.openmailbox.org": {
+    "userContextId": "37",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_app.openmailbox.org": {
+    "userContextId": "37",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.angryhosting.com": {
+    "userContextId": "38",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.namesilo.com": {
+    "userContextId": "40",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.namecheap.com": {
+    "userContextId": "39",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_ap.www.namecheap.com": {
+    "userContextId": "39",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.spotify.com": {
+    "userContextId": "41",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_accounts.spotify.com": {
+    "userContextId": "41",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_support.spotify.com": {
+    "userContextId": "41",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_open.spotify.com": {
+    "userContextId": "41",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.motorola.com": {
+    "userContextId": "42",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_accounts.motorola.com": {
+    "userContextId": "42",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_motorola-global-portal.custhelp.com": {
+    "userContextId": "42",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_support.motorola.com": {
+    "userContextId": "42",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_blog.motorola.com": {
+    "userContextId": "42",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.codecademy.com": {
+    "userContextId": "43",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_discuss.codecademy.com": {
+    "userContextId": "43",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.coinbase.com": {
+    "userContextId": "44",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_poloniex.com": {
+    "userContextId": "45",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_cloud.libretron.com": {
+    "userContextId": "46",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_cloud9.libretron.com": {
+    "userContextId": "47",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.gravie.com": {
+    "userContextId": "48",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_member.gravie.com": {
+    "userContextId": "48",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_member.ucare.org": {
+    "userContextId": "49",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_home.ucare.org": {
+    "userContextId": "49",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_fafsa.ed.gov": {
+    "userContextId": "50",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.mycornerstoneloan.org": {
+    "userContextId": "51",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_myaccount.mycornerstoneloan.org": {
+    "userContextId": "51",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.deltadental.com": {
+    "userContextId": "52",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.blizzard.com": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_us.battle.net": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_us.shop.battle.net": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_news.blizzard.com": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_worldofwarcraft.com": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_playhearthstone.com": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_heroesofthestorm.com": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_playoverwatch.com": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_us.diablo3.com": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_starcraft.com": {
+    "userContextId": "53",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_news.ycombinator.com": {
+    "userContextId": "54",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_hn.algolia.com": {
+    "userContextId": "54",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.linkedin.com": {
+    "userContextId": "55",
+    "neverAsk": true
+  },
+  "identitiesState@@_firefox-container-56": {
+    "hiddenTabs": []
+  },
+  "siteContainerMap@@_www.jetbrains.com": {
+    "userContextId": "56",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_account.jetbrains.com": {
+    "userContextId": "56",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.dollarshaveclub.com": {
+    "userContextId": "57",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.metrotransit.org": {
+    "userContextId": "58",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_account.metrotransit.org": {
+    "userContextId": "58",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_store.metrotransit.org": {
+    "userContextId": "58",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.freecodecamp.org": {
+    "userContextId": "59",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.dropbox.com": {
+    "userContextId": "60",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_forum.freecodecamp.org": {
+    "userContextId": "59",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_informeddelivery.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_reg.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_store.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_tools.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_retail-pi.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_redelivery.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_holdmail.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_moversguide.usps.com": {
+    "userContextId": "61",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_trello.com": {
+    "userContextId": "62",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.microcenter.com": {
+    "userContextId": "63",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_cart.microcenter.com": {
+    "userContextId": "63",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_10.0.20.2": {
+    "userContextId": "64",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_10.0.20.10": {
+    "userContextId": "65",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_dunwoody-csm.symplicity.com": {
+    "userContextId": "66",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_userstyles.org": {
+    "userContextId": "67",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_forum.userstyles.org": {
+    "userContextId": "67",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_cache6.pinboard.in": {
+    "userContextId": "15",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_www.mnsure.org": {
+    "userContextId": "69",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_": {
+    "userContextId": "69",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_id.mneiam.mn.gov": {
+    "userContextId": "69",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_auth.mnsure.org": {
+    "userContextId": "69",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_people.mnsure.org": {
+    "userContextId": "69",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_accounts.firefox.com": {
+    "userContextId": "70",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_gist.githubusercontent.com": {
+    "userContextId": "6",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_screenshots.firefox.com": {
+    "userContextId": "70",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.loverslab.com": {
+    "userContextId": "85",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.instapaper.com": {
+    "userContextId": "86",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_smile.amazon.com": {
+    "userContextId": "8",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.etsy.com": {
+    "userContextId": "87",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_199.66.73.52": {
+    "userContextId": "88",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.tumblr.com": {
+    "userContextId": "89",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_testpilot.firefox.com": {
+    "userContextId": "70",
+    "neverAsk": false
+  },
+  "siteContainerMap@@_jsfiddle.net": {
+    "userContextId": "90",
+    "neverAsk": true
+  },
+  "siteContainerMap@@_www.humblebundle.com": {
+    "userContextId": "91",
+    "neverAsk": false
+  }
+}
