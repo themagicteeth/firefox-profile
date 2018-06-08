1e85000c79f43dc76f8804f34706b22b351d261f
themagicteeth <themagicteeth@users.noreply.github.com>
2018-06-08 17:06:10 +0000

    Added utils, with Windows feature folder script

---
+++ utils/features-clean-windows.bat
@@ -0,0 +1,6 @@
+cd C:\Program Files\Nightly\browser\features
+del firefox@getpocket.com.xpi
+del followonsearch@mozilla.com.xpi
+del formautofill@mozilla.org.xpi
+del onboarding@mozilla.org.xpi
+del webcompat-reporter@mozilla.org.xpi
